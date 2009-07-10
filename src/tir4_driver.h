2a7dee38ac1a6a4000e82fc8be936eebc6f174ed
tulthix <tulthix@733d3608-6b34-11de-ba0c-230037762ff9>
2009-07-10 13:14:41 +0000

    adding in the initial c source tree

---
+++ src/tir4_driver.h
@@ -0,0 +1,11 @@
+#include <usb.h>
+
+void init(void);
+void shutdown(void);
+void set_configuration_tir4();
+void set_altinterface_tir4(void);
+void claim_tir4(void);
+void release_tir4(void);
+void close_tir4(void);
+struct usb_device *find_device(uint16_t, uint16_t);
+void error_alert(char *);
