c3a9d17ccd043aa1e00e193c83b8c6f1c65408d0
f.josef <f.josef@email.cz>
2009-07-14 23:23:30 +0000

    Added pose analu=ysis functions

---
+++ src/math_utils.c
@@ -0,0 +1,130 @@
+
+#include "math_utils.h"
+#include <stdio.h>
+void make_vec(float pt1[3],float pt2[3],float res[3])
+{
+  res[0]=pt1[0]-pt2[0];
+  res[1]=pt1[1]-pt2[1];
+  res[2]=pt1[2]-pt2[2];
+}
+
+float vec_size(float vec[3])
+{
+  return(sqrt(vec[0]*vec[0]+vec[1]*vec[1]+vec[2]*vec[2]));
+}
+
+float dot_product(float vec1[3],float vec2[3])
+{
+  return(vec1[0]*vec2[0]+vec1[1]*vec2[1]+vec1[2]*vec2[2]);
+}
+
+void cross_product(float vec1[3],float vec2[3],float res[3])
+{
+  res[0]=vec1[1]*vec2[2]-vec1[2]*vec2[1];
+  res[1]=vec1[2]*vec2[0]-vec1[0]*vec2[2];
+  res[2]=vec1[0]*vec2[1]-vec1[1]*vec2[0];
+}
+
+void mul_vec(float vec[3],float c,float res[3])
+{
+  res[0]=vec[0]*c;
+  res[1]=vec[1]*c;
+  res[2]=vec[2]*c;
+}
+
+void make_base(float vec1[3],float vec2[3],float res[3][3])
+{
+  float tmp1[3],tmp2[3];
+  mul_vec(vec1,1/vec_size(vec1),res[0]);
+  mul_vec(res[0],dot_product(vec2,res[0]),tmp1);
+  make_vec(vec2,tmp1,tmp2);
+  mul_vec(tmp2,1/vec_size(tmp2),res[1]);
+  cross_product(res[0],res[1],res[2]);
+  
+}
+
+void matrix_times_vec(float m[3][3], float vec[3],float res[3])
+{
+  res[0] = dot_product(m[0], vec);
+  res[1] = dot_product(m[1], vec);
+  res[2] = dot_product(m[2], vec);
+}
+
+void print_matrix(float matrix[3][3], char *name)
+{
+  int i,j;
+  printf("\n%s = {\n", name);
+  for(i = 0; i < 3; ++i){
+    for(j = 0; j < 3; ++j){
+      printf("%15f,", matrix[i][j]);
+    }
+    printf("\n");
+  }
+  printf("}\n");
+}
+
+void print_vec(float vec[3], char *name)
+{
+  printf("%s = {%15f; %15f; %15f}\n", name, vec[0], vec[1], vec[2]);
+}
+
+float sqr(float f)
+{
+  return(f*f);
+}
+
+void mul_matrix(float m1[3][3], float m2[3][3], float res[3][3])
+{
+  int i,j;
+  float m2t[3][3];
+  transpose(m2, m2t);
+  for(i = 0; i < 3; ++i){
+    for(j = 0; j < 3; ++j){
+      res[i][j] = dot_product(m1[i], m2t[j]);
+    }
+  }
+}
+
+void transpose(float matrix[3][3], float trans[3][3])
+{
+  int i,j;
+  for(i = 0; i < 3; ++i){
+    for(j = 0; j < 3; ++j){
+      trans[i][j] = matrix[j][i];
+    }
+  }
+}
+
+void swp(float *f1, float *f2)
+{
+  float tmp;
+  tmp = *f1;
+  *f1 = *f2;
+  *f2 = tmp;
+}
+
+void transpose_in_place(float matrix[3][3])
+{
+  swp(&matrix[0][1], &matrix[1][0]);
+  swp(&matrix[0][2], &matrix[2][0]);
+  swp(&matrix[1][2], &matrix[2][1]);
+}
+
+void matrix_to_euler(float matrix[3][3], float *pitch, float *yaw, float *roll)
+{
+  float tmp = matrix[0][2];
+  if(tmp < -1.0f) 
+    tmp = -1.0f;
+  if(tmp > 1.0f)
+    tmp = 1.0f;
+  *yaw = -asinf(tmp);
+  float yc = cosf(*yaw);
+  if (fabs(yc) > 1e-5){
+    *pitch = atan2f(-matrix[1][2]/yc, matrix[2][2]/yc);
+    *roll = atan2f(-matrix[0][1]/yc, matrix[0][0]/yc);
+  }else{
+    *pitch = 0.0f;
+    *roll = atan2f(matrix[1][0], matrix[1][1]);
+  }
+}
+
---
+++ src/pose.c
@@ -0,0 +1,204 @@
+
+
+#include <stdio.h>
+#include "pose.h"
+#include "math_utils.h"
+#include "cal.h"
+
+/* Focal length */
+float focal_length = 10;
+
+float model_point0[3];
+float model_point1[3];
+float model_point2[3];
+
+float model_ref[3];
+
+float model_base[3][3];
+
+float center_ref[3];
+float center_base[3][3];
+
+int center_flag = 0;
+
+void init_model()
+{
+  /* Physical dimensions */
+  /* Camera looks in direction of Z axis */
+  /* X axis goes then to the left */
+  /* Y axis goes up */
+  float x = 168;
+  float y = 90;
+  float z = 100;
+  
+  /* Point, around which model rotates (just dimensions)*/
+  float ref_y = 150;
+  float ref_z = 150;
+  float ref[3];
+  
+  model_point0[0] = 0.0; 
+  model_point0[1] = y; 
+  model_point0[2] = z;
+  
+  model_point1[0] = -x/2; 
+  model_point1[1] = 0.0; 
+  model_point1[2] = 0.0;
+  
+  model_point2[0] = x/2; 
+  model_point2[1] = 0.0; 
+  model_point2[2] = 0.0;
+  
+  ref[0] = 0.0; 
+  ref[1] = -ref_y; 
+  ref[2] = ref_z;
+  
+  /* Out of model points create orthonormal base */
+  float vec1[3];
+  float vec2[3];
+  make_vec(model_point1, model_point0, vec1);
+  make_vec(model_point2, model_point0, vec2);
+  make_base(vec1, vec2, model_base);
+
+//for testing purposes
+  make_base(vec1, vec2, center_base);
+  
+  /* Convert reference point to model base coordinates */
+  float ref_pt[3];
+  float vec3[3];
+  make_vec(ref, model_point0, vec3);
+  matrix_times_vec(model_base, vec3, model_ref);
+}
+
+float blob_dist(struct blob_type b0, struct blob_type b1)
+{
+  return sqrt(sqr(b1.x-b0.x) + sqr(b1.y-b0.y));
+}
+
+void alter_pose(struct bloblist_type blobs, float points[3][3])
+{
+  float R01, R02, R12, d01, d02, d12, a, b, c, s, h1, h2, sigma;
+  float tmp[3];
+  
+  make_vec(model_point0, model_point1, tmp);
+  R01 = vec_size(tmp);
+  make_vec(model_point0, model_point2, tmp);
+  R02 = vec_size(tmp);
+  make_vec(model_point1, model_point2, tmp);
+  R12 = vec_size(tmp);
+  
+  d01 = blob_dist(blobs.blobs[0], blobs.blobs[1]);
+  d02 = blob_dist(blobs.blobs[0], blobs.blobs[2]);
+  d12 = blob_dist(blobs.blobs[1], blobs.blobs[2]);
+
+  a = (R01 + R02+ R12) * (-R01 + R02 + R12)
+    * (R01 - R02 + R12) * (R01 + R02 - R12);
+    
+  b = sqr(d01) * (-sqr(R01) + sqr(R02) + sqr(R12)) 
+    + sqr(d02) * (sqr(R01) - sqr(R02) + sqr(R12)) 
+    + sqr(d12) * (sqr(R01) + sqr(R02) - sqr(R12));
+    
+  c = (d01 + d02+ d12) * (-d01 + d02 + d12) 
+    * (d01 - d02 + d12) * (d01 + d02 - d12);
+  
+  s = sqrt((b + sqrt(sqr(b) - a * c)) / a);
+  
+  if((sqr(d01) + sqr(d02) - sqr(d12)) 
+      <= (sqr(s) * (sqr(R01) + sqr(R02) - sqr(R12)))){
+    sigma = 1;
+  }else{
+    sigma = -1;
+  } 
+
+  h1 = -sqrt(sqr(s * R01) - sqr(d01));
+  h2 = -sigma * sqrt(sqr(s * R02) - sqr(d02));
+  
+  points[0][0] = blobs.blobs[0].x / s;
+  points[1][0] = blobs.blobs[0].y / s;
+  points[2][0] = focal_length / s;
+  
+  points[0][1] = blobs.blobs[1].x / s;
+  points[1][1] = blobs.blobs[1].y / s;
+  points[2][1] = (focal_length + h1) / s;
+  
+  points[0][2] = blobs.blobs[2].x / s;
+  points[1][2] = blobs.blobs[2].y / s;
+  points[2][2] = (focal_length + h2) / s;
+  
+}
+
+void get_translation(float base[3][3], float ref[3], float origin[3], 
+                                                        float trans[3]){
+  float tmp[3];
+  float t_base[3][3];
+  float new_ref[3];
+  transpose(base, t_base);
+  matrix_times_vec(t_base, ref, new_ref);
+  new_ref[0] += origin[0];
+  new_ref[1] += origin[1];
+  new_ref[2] += origin[2];
+//  print_vec(new_ref, "new_ref");
+//  print_vec(origin, "origin");
+  trans[0] = new_ref[0] - center_ref[0];
+  trans[1] = new_ref[1] - center_ref[1];
+  trans[2] = new_ref[2] - center_ref[2];
+}
+
+void get_transform(float new_base[3][3], float rot[3][3]){
+  float center_base_t[3][3], new_base_t[3][3];
+  transpose(center_base, center_base_t);   
+  mul_matrix(center_base_t, new_base, rot);
+}
+
+bool process_blobs(struct bloblist_type blobs, struct transform *trans)
+{
+  
+  float points[3][3];
+//  print_matrix(model_base, "Model_base");
+//  print_vec(model_ref, "Ref_point");
+  alter_pose(blobs, points);
+//  print_matrix(points, "Alter");
+
+  float new_base[3][3];
+  float vec1[3];
+  float vec2[3];
+  transpose_in_place(points);
+//  print_matrix(points, "Alter");
+  make_vec(points[1], points[0], vec1); 
+  make_vec(points[2], points[0], vec2);
+//  print_vec(vec1, "vec1");
+//  print_vec(vec2, "vec2");
+  make_base(vec1, vec2, new_base);
+//  print_matrix(new_base, "New_base");
+  float new_ref[3];
+  get_translation(new_base, model_ref, points[0], trans->tr);
+//  print_vec(trans->tr, "translation");
+  get_transform(new_base, trans->rot);
+//  print_matrix(trans->rot, "Rot");
+  return true;
+}
+
+/*
+int main(void)
+{
+  init_model();
+  
+  struct blob_type bl[3];
+  struct bloblist_type blobs;
+  struct transform res;
+  blobs.num_blobs = 3;
+  blobs.blobs = bl;
+  blobs.blobs[0].x = 0;
+  blobs.blobs[0].y = 2.52632;
+  blobs.blobs[1].x = -0.98824;
+  blobs.blobs[1].y = 1.76471;
+  blobs.blobs[2].x = 0.98824;
+  blobs.blobs[2].y = 1.76471;
+  process_blobs(blobs ,&res);
+  
+  float pitch, roll, yaw;
+  matrix_to_euler(res.rot, &pitch, &yaw, &roll);
+  printf("Pitch: %f  Yaw: %f  Roll: %f\n", pitch, yaw, roll);
+  
+  return 0;
+}
+*/
