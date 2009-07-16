b2562cf4d4b3ac823568842f8e9c1be35d1f54ce
tulthix <tulthix@733d3608-6b34-11de-ba0c-230037762ff9>
2009-07-16 03:13:36 +0000

    Added a simple tir4 pose_test program.

---
+++ trunk/src/pose_test.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "cal.h"
+#include "tir4_driver.h"
+#include "pose.h"
+
+int main(int argc, char **argv) {
+  struct frame_type frame;
+  struct camera_control_block ccb;
+  struct transform t;
+
+  ccb.device.category = tir4_camera;
+/*   ccb.mode = diagnostic; */
+/*   ccb.mode = operational_1dot; */
+  ccb.mode = operational_3dot;
+
+  cal_init(&ccb);
+
+  init_model();
+
+  /* call below sets led green */
+  cal_set_good_indication(&ccb, true);
+
+  /* loop forever reading and printing results */
+/*   while (true) { */
+/*     cal_get_frame(&ccb, &frame); */
+/*     frame_print(frame); */
+/*     frame_free(&ccb, &frame); */
+/*   } */
+  
+  int i;
+  for(i=0; i<2000; i++) {
+    cal_get_frame(&ccb, &frame);
+    frame_print(frame);
+    process_blobs(frame.bloblist, &t);
+    print_transform(t);
+    frame_free(&ccb, &frame);
+  }
+
+  /* call disconnects, turns all LEDs off */
+  cal_shutdown(&ccb);
+  return 0;
+}
