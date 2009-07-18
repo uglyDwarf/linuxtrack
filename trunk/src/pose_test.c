5577efbb6f5bfbb334671bf813f7ed3bc2d04a72
tulthix <tulthix@733d3608-6b34-11de-ba0c-230037762ff9>
2009-07-18 18:22:10 +0000

    Okay, this version seems to actually do the tracking, and quite well.  Only tracking problem is X and Y angles are mirrored.  Translations and roll angle looks right.  Other problem is it segfaults if tir4 init fails.

--- trunk/src/pose_test.c
+++ trunk/src/pose_test.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
 /*   ccb.mode = operational_1dot; */
   ccb.mode = operational_3dot;
 
-/*   cal_init(&ccb); */
+  cal_init(&ccb);
 
   rm.p1[0] = -35.0;
   rm.p1[1] = -50.0;
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
   pose_init(rm, 0.0);
 
   /* call below sets led green */
-/*   cal_set_good_indication(&ccb, true); */
+  cal_set_good_indication(&ccb, true);
 
   /* loop forever reading and printing results */
 /*   while (true) { */
@@ -38,41 +38,46 @@ int main(int argc, char **argv) {
 /*     frame_free(&ccb, &frame); */
 /*   } */
 
-  /* NEW PLAN */
-  /* NEW PLAN */
-  /* NEW PLAN */
-  /* going to read in from stdin simulated blobs. 
-   * echoing input blob out, along with alter92 
-   * estimates */
-  struct bloblist_type working_bloblist;
-  struct blob_type wba[3];
-  bool force_recenter = false;
-  int force_recenter_int;
-  working_bloblist.num_blobs = 3;
-  working_bloblist.blobs = wba;
-  while (true) {
-    printf("Enter blobs: ");
-    scanf("%f,%f,%f,%f,%f,%f,%d",
-           &(wba[0].x), &(wba[0].y),
-           &(wba[1].x), &(wba[1].y),
-           &(wba[2].x), &(wba[2].y),
-           &force_recenter_int);
-    printf("Received: ");
-    printf("%f,%f,", (wba[0].x), (wba[0].y));
-    printf("%f,%f,", (wba[1].x), (wba[1].y));
-    printf("%f,%f,", (wba[2].x), (wba[2].y));
-    printf("%d\n", force_recenter_int);
-    pose_process_blobs(working_bloblist, &t);
-    transform_print(t);
-  }
+/*   /\* NEW PLAN *\/ */
+/*   /\* NEW PLAN *\/ */
+/*   /\* NEW PLAN *\/ */
+/*   /\* going to read in from stdin simulated blobs.  */
+/*    * echoing input blob out, along with alter92  */
+/*    * estimates *\/ */
+/*   struct bloblist_type working_bloblist; */
+/*   struct blob_type wba[3]; */
+/*   bool force_recenter = false; */
+/*   int force_recenter_int; */
+/*   int scanf_result; */
+/*   working_bloblist.num_blobs = 3; */
+/*   working_bloblist.blobs = wba; */
+/*   while (!feof(stdin)) { */
+/* /\*     printf("Enter blobs: "); *\/ */
+/*     scanf_result=scanf("%f,%f,%f,%f,%f,%f,%d", */
+/*                        &(wba[0].x), &(wba[0].y), */
+/*                        &(wba[1].x), &(wba[1].y), */
+/*                        &(wba[2].x), &(wba[2].y), */
+/*                        &force_recenter_int); */
+/*     if (scanf_result == 7) { */
+/* /\*       printf("Received: "); *\/ */
+/* /\*       printf("%f,%f,", (wba[0].x), (wba[0].y)); *\/ */
+/* /\*       printf("%f,%f,", (wba[1].x), (wba[1].y)); *\/ */
+/* /\*       printf("%f,%f,", (wba[2].x), (wba[2].y)); *\/ */
+/* /\*       printf("%d\n", force_recenter_int); *\/ */
+/*       pose_process_blobs(working_bloblist, &t); */
+/*       transform_print(t); */
+/*     } */
+/*   } */
+
 /*   int i; */
 /*   for(i=0; i<2000; i++) { */
-/*     cal_get_frame(&ccb, &frame); */
-/*     frame_print(frame); */
-/*     pose_process_blobs(frame.bloblist, &t); */
-/*     transform_print(t); */
-/*     frame_free(&ccb, &frame); */
-/*   } */
+  while (true) {
+    cal_get_frame(&ccb, &frame);
+    frame_print(frame);
+    pose_process_blobs(frame.bloblist, &t);
+    transform_print(t);
+    frame_free(&ccb, &frame);
+  }
 
   /* call disconnects, turns all LEDs off */
 /*   cal_shutdown(&ccb); */
