7ddba9e216f3d20abb16163890b5d7cb27563b72
f.josef <f.josef@email.cz>
2009-07-18 00:06:40 +0000

    Added check for presence of libcwiid to configure; added threading functions to cal.c - I have no way, how to test it properly, so use caution.

--- src/cal.c
+++ src/cal.c
@@ -3,7 +3,10 @@
  *************************************************************/
 #include <stdbool.h>
 #include <stdio.h>
+#include <pthread.h>
+
 #include "cal.h"
+#include "pose.h"
 #include "tir4_driver.h"
 #include "wiimote_driver.h"
 
@@ -171,3 +174,92 @@ void blob_print(struct blob_type b)
   printf("x: %f\ty: %f\tscore: %d\n", b.x,b.y,b.score);
 }
 
+/* Thread implementation*/
+
+//Helper function that allows me to copy transform between different sourcs
+void copy_transform(struct transform *src, struct transform *dest)
+{
+  int i,j;
+  for(i = 0; i < 3; ++i){
+    dest->tr[i] = src->tr[i];
+  }
+
+  for(i = 0; i < 3; ++i){
+    for(j = 0; i<3; ++i){
+      dest->rot[i][j] = src->rot[i][j];
+    }
+  }
+  
+}
+
+
+pthread_t capture_thread_id;
+//Setting this to false couses thread exit loop 
+volatile bool capture_stop_flag = false;
+
+//Those are last data from hardware (processed)
+struct transform last_transform;
+pthread_mutex_t last_data_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+//Main function of capture thread
+void *capture_thread(void *ccb)
+{
+  struct frame_type frame;
+  struct transform t;
+  while(capture_stop_flag != true){
+    cal_get_frame(ccb, &frame);
+    pose_process_blobs(frame.bloblist, &t);
+
+    pthread_mutex_lock(&last_data_mutex);
+    copy_transform(&t, &last_transform);
+    pthread_mutex_unlock(&last_data_mutex);
+    frame_free(ccb, &frame);
+  }
+  capture_stop_flag = false;
+  return NULL;
+}
+
+//Access function meant to be used from outside
+void get_current_transform(struct transform *res)
+{
+  static struct transform data = {
+    .tr = {0.0f, 0.0f, 0.0f},
+    .rot = {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}}
+  };
+  
+  /*
+  If we get the mutex, we are good to go and read the data...
+  If not, just output last data.
+  */
+  if(pthread_mutex_trylock(&last_data_mutex) == 0){
+    //we hold the lock now!
+    copy_transform(&last_transform, &data);
+    pthread_mutex_unlock(&last_data_mutex);
+  }
+  copy_transform(&data, res);
+}
+
+//Start the capture thread
+bool thread_start()
+{
+  //Check if there is not already request to stop...
+  if(capture_stop_flag == true){
+    return false;
+  }
+
+  capture_stop_flag = false;
+  int res = pthread_create(&capture_thread_id, NULL, 
+                           capture_thread, (void*)NULL);
+  if(res != 0){
+    printf("Can't create capture thread!\n");
+    return false;
+  }
+  return true;
+}
+
+//Request termination of capture thread
+void thread_stop()
+{
+  capture_stop_flag = true;
+}
