0788bbdddc6e903f08dc2dd29018b3e9cda87155
tulthix <tulthix@733d3608-6b34-11de-ba0c-230037762ff9>
2009-07-19 23:46:18 +0000

    big update, threading used now, see ltlib for cal thread usage.

--- src/cal.c
+++ src/cal.c
@@ -3,11 +3,10 @@
  *************************************************************/
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 #include "cal.h"
-#include "pose.h"
-#include "tir4_driver.h"
 #include "wiimote_driver.h"
 
 /*********************/
@@ -19,6 +18,8 @@
 /* private Static members */
 /**************************/
 /* none */
+
+void *capture_thread(void *ccb);
 
 /************************/
 /* function definitions */
@@ -74,17 +75,17 @@ int cal_suspend(struct camera_control_block *ccb)
 }
 
 void cal_change_operating_mode(struct camera_control_block *ccb,
-                               enum cal_operating_mode newmode)
+                              enum cal_operating_mode newmode)
 {
   switch (ccb->device.category) {
   case tir4_camera:
-    return tir4_change_operating_mode(ccb,newmode);
+    tir4_change_operating_mode(ccb,newmode);
     break;
   case webcam:
 /*     return webcam_change_operating_mode(ccb,newmode); */
     break;
   case wiimote:
-     return wiimote_change_operating_mode(ccb,newmode); 
+    wiimote_change_operating_mode(ccb,newmode); 
     break;
   }
 }
@@ -123,7 +124,7 @@ int cal_set_good_indication(struct camera_control_block *ccb,
 }
 
 int cal_get_frame(struct camera_control_block *ccb,
-                  struct frame_type *f)
+                         struct frame_type *f)
 {
   switch (ccb->device.category) {
   case tir4_camera:
@@ -175,91 +176,160 @@ void blob_print(struct blob_type b)
 }
 
 /* Thread implementation*/
-
-//Helper function that allows me to copy transform between different sourcs
-void copy_transform(struct transform *src, struct transform *dest)
-{
-  int i,j;
-  for(i = 0; i < 3; ++i){
-    dest->tr[i] = src->tr[i];
-  }
-
-  for(i = 0; i < 3; ++i){
-    for(j = 0; i<3; ++i){
-      dest->rot[i][j] = src->rot[i][j];
-    }
-  }
-  
-}
-
-
 pthread_t capture_thread_id;
-//Setting this to false couses thread exit loop 
-volatile bool capture_stop_flag = false;
 
 //Those are last data from hardware (processed)
-struct transform last_transform;
-pthread_mutex_t last_data_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t pending_frame_mutex = PTHREAD_MUTEX_INITIALIZER;
+/* guarded by pending_frame_mutex: { */
+static struct frame_type pending_frame;
+static bool pending_frame_valid = false;
+/* } */
+
+pthread_mutex_t capture_thread_mutex = PTHREAD_MUTEX_INITIALIZER;
+/* guarded by capture_thread_mutex: { */
+static bool pending_capture_state_active = false;
+static bool current_capture_state_active = false;
+/* } */
+
+/* Start the capture thread, this WILL BLOCK until the 
+ * thread has actually started; this should be a very short 
+ * period */
+void cal_thread_start(struct camera_control_block *ccb)
+{
+  /* check if there is a capture thread running, 
+   * and handle this oddball case:
+   *   - capture thread was previously started, 
+   *   - then it was told to stop, however,
+   *     - there were no frames, so its hung, unable to actually stop.
+   *   - we have called it to start again, so lets just clear the stop 
+   *     signal and return.
+   * In the typo case of starting the thread twice, well,
+   * we'll just continue running*/
+  bool already_running = false;
+  pthread_mutex_lock(&capture_thread_mutex);
+  if(current_capture_state_active){
+    already_running = true;
+    pending_capture_state_active = true;
+  }
+  pthread_mutex_unlock(&capture_thread_mutex);
+  if (already_running) {
+    return;
+  }
+  /* so no thread is running, we can access all shared
+   * variables freely right now */
+  pending_frame_valid = false;
+  current_capture_state_active = false;
+  pending_capture_state_active = true;
+  int res = pthread_create(&capture_thread_id, NULL, 
+                           capture_thread, (void*)ccb);
+  if(res != 0){
+    fprintf(stderr, "Can't create capture thread!\n");
+    exit(1);
+  }
+  /* thread created, to keep things simple, we block until
+   * it has started executing */ 
+  bool done = false;
+  while (!done) {
+    pthread_mutex_lock(&capture_thread_mutex);
+    if(current_capture_state_active){
+      done = true;
+    }
+    pthread_mutex_unlock(&capture_thread_mutex);
+  }
+}
+
+/* Request termination of capture thread
+ * note that the thread may not actually stop if its 
+ * hung up looking for a frame.  It will stop when the 
+ * it gets a chance though.  use the cal_thread_is_stopped()
+ * below to poll until its really stopped, if needed */ 
+void cal_thread_stop(void)
+{
+  /* tell it to die */
+  pthread_mutex_lock(&capture_thread_mutex);
+  pending_capture_state_active = false;
+  pthread_mutex_unlock(&capture_thread_mutex);
+}
+
+/* returns true if the thread is actually stopped */
+bool cal_thread_is_stopped(void)
+{
+  /* did it actually die? */
+  bool it_died=false;
+  pthread_mutex_lock(&capture_thread_mutex);
+  if(current_capture_state_active == false){
+    it_died = true;
+  }
+  pthread_mutex_unlock(&capture_thread_mutex);
+  return it_died;
+}
+
+/* Access function meant to be used from outside
+ * return_frame is assumed to be a freed frame, 
+ * (no bitmap or blobs allocated). 
+ * be sure to free the frame returned when done with it! */
+void cal_thread_get_frame(struct frame_type *return_frame, 
+                          bool *return_frame_valid)
+{
+  if(pthread_mutex_trylock(&pending_frame_mutex) == 0){
+    //we hold the lock now!
+    if (pending_frame_valid) {
+      *return_frame_valid = true;
+      pending_frame_valid = false;
+      *return_frame = pending_frame;
+    }
+    else {
+      *return_frame_valid = false;
+    }
+    pthread_mutex_unlock(&pending_frame_mutex);
+  }
+  else {
+    *return_frame_valid = false;
+  }
+}
 
 
 //Main function of capture thread
 void *capture_thread(void *ccb)
 {
   struct frame_type frame;
-  struct transform t;
-  while(capture_stop_flag != true){
-    cal_get_frame(ccb, &frame);
-    pose_process_blobs(frame.bloblist, &t);
-
-    pthread_mutex_lock(&last_data_mutex);
-    copy_transform(&t, &last_transform);
-    pthread_mutex_unlock(&last_data_mutex);
-    frame_free(ccb, &frame);
+  /* on entry, we signal that we are running */
+  pthread_mutex_lock(&capture_thread_mutex);
+  current_capture_state_active = true;
+  pthread_mutex_unlock(&capture_thread_mutex);
+  /* the run until told to stop loop */
+  bool terminate_flag = false;
+  while (!terminate_flag) {
+    /* watch for a signal to flag termination */
+    pthread_mutex_lock(&capture_thread_mutex);
+    if(pending_capture_state_active == false){
+      terminate_flag = true;
+    }
+    pthread_mutex_unlock(&capture_thread_mutex);
+    if (!terminate_flag) {
+      cal_get_frame(ccb, &frame);
+      pthread_mutex_lock(&pending_frame_mutex);
+      /* if there is an unread valid frame,
+       * we free it before overwriting it */
+      if (pending_frame_valid) {
+        frame_free(ccb, &pending_frame);
+      }
+      pending_frame_valid = true;
+      pending_frame = frame;
+      pthread_mutex_unlock(&pending_frame_mutex);
+    }
   }
-  capture_stop_flag = false;
+  pthread_mutex_lock(&pending_frame_mutex);
+  /* if there is final unread valid frame,
+   * we free it before terminating */
+  if (pending_frame_valid) {
+    frame_free(ccb, &pending_frame);
+  }
+  pending_frame_valid = false;
+  pthread_mutex_unlock(&pending_frame_mutex);
+
+  pthread_mutex_lock(&capture_thread_mutex);
+  current_capture_state_active = false;
+  pthread_mutex_unlock(&capture_thread_mutex);
   return NULL;
-}
-
-//Access function meant to be used from outside
-void get_current_transform(struct transform *res)
-{
-  static struct transform data = {
-    .tr = {0.0f, 0.0f, 0.0f},
-    .rot = {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}}
-  };
-  
-  /*
-  If we get the mutex, we are good to go and read the data...
-  If not, just output last data.
-  */
-  if(pthread_mutex_trylock(&last_data_mutex) == 0){
-    //we hold the lock now!
-    copy_transform(&last_transform, &data);
-    pthread_mutex_unlock(&last_data_mutex);
-  }
-  copy_transform(&data, res);
-}
-
-//Start the capture thread
-bool thread_start()
-{
-  //Check if there is not already request to stop...
-  if(capture_stop_flag == true){
-    return false;
-  }
-
-  capture_stop_flag = false;
-  int res = pthread_create(&capture_thread_id, NULL, 
-                           capture_thread, (void*)NULL);
-  if(res != 0){
-    printf("Can't create capture thread!\n");
-    return false;
-  }
-  return true;
-}
-
-//Request termination of capture thread
-void thread_stop()
-{
-  capture_stop_flag = true;
 }
