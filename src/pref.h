7542da429e43cdaa99e3bffc27d6f914649d5cec
f.josef <f.josef@email.cz>
2009-07-21 21:41:58 +0000

    Updated plugin centering function; added functions to change preference, dump prefs to file and to free preferences.

--- src/pref.h
+++ src/pref.h
@@ -7,5 +7,8 @@ bool section_exists(char *section_name);
 bool key_exists(char *section_name, char *key_name);
 char *get_key(char *section_name, char *key_name);
 
+bool change_key(char *section_name, char *key_name, char *new_value);
+bool dump_prefs(char *file_name);
+void free_prefs();
 
 #endif
