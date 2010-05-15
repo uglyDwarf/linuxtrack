3e71f16fb0de1bfe4d596df1f78247665ef8f291
f.josef@email.cz <f.josef@email.cz@733d3608-6b34-11de-ba0c-230037762ff9>
2010-05-15 22:50:39 +0000

    Made first tab default in the GUI. Resolved the locale bug causing malformed numbers.

--- branches/michal/src/qt_gui/main.cpp
+++ branches/michal/src/qt_gui/main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
 
 #include "ltr_gui.h"
+#include <locale.h>
 
  int main(int argc, char *argv[])
  {
      QApplication app(argc, argv);
+     setlocale(LC_NUMERIC,"C");
      LinuxtrackGui gui;
      gui.show();
      return app.exec();
