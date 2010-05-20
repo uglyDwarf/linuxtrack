491041900d5a4b37c9796ce1fc3b5cd01804a069
tulthix <tulthix@733d3608-6b34-11de-ba0c-230037762ff9>
2010-05-20 02:54:35 +0000

    ltcomm package cleanup

--- src/ltcomm.h
+++ src/ltcomm.h
@@ -2,10 +2,32 @@
 #define LTCOMM__H 1
 
 #include <stdint.h>
+/* for network */
+#ifndef WIN32
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#else
+#include <winsock2.h>
+#endif
 
+/*** COMMON ***/
+/*** COMMON ***/
 #define LTCOMM_DEFAULT_CLIENT_PORT 12012
 #define LTCOMM_DEFAULT_SERVER_PORT 12013
 #define LTCOMM_DEFAULT_HOST        "127.0.0.1"
+
+typedef struct {
+  int                 sd;
+#ifndef WIN32
+  struct  sockaddr_in server_addr;
+  struct  sockaddr_in client_addr;
+#else
+  SOCKADDR_IN server_addr;
+  SOCKADDR_IN client_addr;
+#endif
+} ltcomm_net_ctrl_blk_t;
 
 /*** CLIENT INITIATE MESSAGES ***/
 /*** CLIENT INITIATE MESSAGES ***/
@@ -20,10 +42,23 @@
 #define LTCOMM_CID_DISCONNECT                  109
 #define LTCOMM_CID_TERMINATE                   110
 
+#define LTCOMM_CID2STR(x,s) do { x == LTCOMM_CID_CONNECT                     ? strcpy(s,"LTCOMM_CID_CONNECT") : \
+      x                            == LTCOMM_CID_QUERY_VERSION               ? strcpy(s,"LTCOMM_CID_QUERY_VERSION") : \
+      x                            == LTCOMM_CID_GET_PARAM                   ? strcpy(s,"LTCOMM_CID_GET_PARAM") : \
+      x                            == LTCOMM_CID_START_DATA                  ? strcpy(s,"LTCOMM_CID_START_DATA") : \
+      x                            == LTCOMM_CID_REGISTER_PROGRAM_PROFILE_ID ? strcpy(s,"LTCOMM_CID_REGISTER_PROGRAM_PROFILE_ID") : \
+      x                            == LTCOMM_CID_STOP_DATA                   ? strcpy(s,"LTCOMM_CID_STOP_DATA") : \
+      x                            == LTCOMM_CID_RECENTER                    ? strcpy(s,"LTCOMM_CID_RECENTER") : \
+      x                            == LTCOMM_CID_GET_DATA                    ? strcpy(s,"LTCOMM_CID_GET_DATA") : \
+      x                            == LTCOMM_CID_DISCONNECT                  ? strcpy(s,"LTCOMM_CID_DISCONNECT") : \
+      x                            == LTCOMM_CID_TERMINATE                   ? strcpy(s,"LTCOMM_CID_TERMINATE") : \
+      strcpy(s,"UNKNOWN");                                              \
+  } while (0);
+
 typedef struct {
   uint8_t  id;
   uint16_t body; // optional, only used for register_prog_id and get_param
-}  npcomm_client_request_msg_t ;
+}  ltcomm_client_request_msg_t ;
 
 /*** SERVER REPLY MESSAGES ***/
 /*** SERVER REPLY MESSAGES ***/
@@ -32,6 +67,15 @@ typedef struct {
 #define  LTCOMM_SID_PARAM_REPLY   203 // SUCCESS with param for get_param
 #define  LTCOMM_SID_ACK           204 // SUCCESS for others
 #define  LTCOMM_SID_NACK          205 // FAILED
+
+#define LTCOMM_SID2STR(x,s) do { x == LTCOMM_SID_DATA_REPLY    ? strcpy(s,"LTCOMM_SID_DATA_REPLY") : \
+      x                            == LTCOMM_SID_VERSION_REPLY ? strcpy(s,"LTCOMM_SID_VERSION_REPLY") : \
+      x                            == LTCOMM_SID_PARAM_REPLY   ? strcpy(s,"LTCOMM_SID_PARAM_REPLY") : \
+      x                            == LTCOMM_SID_ACK           ? strcpy(s,"LTCOMM_SID_ACK") : \
+      x                            == LTCOMM_SID_NACK          ? strcpy(s,"LTCOMM_SID_NACK") : \
+      strcpy(s,"UNKNOWN");                                              \
+  } while (0);
+
 
 #define  LTCOMM_ERR_INVALID_MSG 001
 
@@ -42,16 +86,18 @@ typedef struct {
   float tx;
   float ty;
   float tz;
-} npcomm_get_data_coords_t;
+} ltcomm_get_data_coords_t;
 
 typedef union {
-  npcomm_get_data_coords_t get_data_body;
-  uint32_t                 nondata_body;
-} npcomm_server_reply_body_t;
+  ltcomm_get_data_coords_t data_body;
+  uint32_t                 version_body;
+  uint32_t                 param_body;
+  uint32_t                 error_body;
+} ltcomm_server_reply_body_t;
 
 typedef struct {
   uint8_t                    id;
-  npcomm_server_reply_body_t body;
-}  npcomm_server_reply_msg_t ;
+  ltcomm_server_reply_body_t body;
+}  ltcomm_server_reply_msg_t ;
 
 #endif
--- src/ltserver_testclient.c
+++ src/ltserver_testclient.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -9,39 +10,122 @@
 #include <string.h>
 #include "ltcomm.h"
 
+#define NET_MAX_RETRIES 20
+
+int ltclient_send_msg(ltcomm_net_ctrl_blk_t       *pnet,
+                      ltcomm_client_request_msg_t *msg)
+{
+  printf("sending msg, id=%d\tHost=%s\tPort=%d\n", msg->id,
+         inet_ntoa(pnet->server_addr.sin_addr),
+         ntohs(pnet->server_addr.sin_port));
+
+  if (sendto(pnet->sd, (void *) msg, sizeof(ltcomm_client_request_msg_t), 0,
+             (struct sockaddr *) &(pnet->server_addr),
+             sizeof(pnet->server_addr)) == -1) {
+    printf("Network sendto failed: id=%d, \n", msg->id);
+    printf("errno: %d\n",errno);
+    printf("read error: %s\n",strerror(errno));
+    return -1;
+  }
+  return 0;
+}
+
+int ltclient_get_reply(ltcomm_net_ctrl_blk_t     *pnet,
+                       ltcomm_server_reply_msg_t *msg)
+{
+  int client_slen = sizeof(struct sockaddr_in);
+
+  if (recvfrom(pnet->sd, (void *) msg, sizeof(ltcomm_server_reply_msg_t), 0,
+               (struct sockaddr *) &(pnet->client_addr),
+               (socklen_t * )&client_slen) == -1) {
+    printf("Network recvfrom failed\n");
+    printf("errno: %d\n",errno);
+    printf("read error: %s\n",strerror(errno));
+    return -1;
+  }
+  return 0;
+}
+
+int ltclient_send_msg_and_get_reply(ltcomm_net_ctrl_blk_t       *pnet,
+                                    ltcomm_client_request_msg_t *txmsg,
+                                    ltcomm_server_reply_msg_t   *rxmsg)
+{
+  if (ltclient_send_msg(pnet, txmsg) == -1) {
+    return -1;
+  }
+  if (ltclient_get_reply(pnet, rxmsg) == -1) {
+    return -1;
+  }
+  return 0;
+}
+
+int ltclient_send_msg_with_retries(ltcomm_net_ctrl_blk_t       *pnet,
+                                   ltcomm_client_request_msg_t *txmsg,
+                                   ltcomm_server_reply_msg_t   *rxmsg,
+                                   uint8_t                      expected_id)
+{
+  uint16_t tries = 0;
+
+  while (tries < NET_MAX_RETRIES) {
+    if (ltclient_send_msg_and_get_reply(pnet, txmsg, rxmsg) == -1) {
+      return -1;
+    }
+    else if (rxmsg->id == expected_id) {
+      return 0;
+    }
+    else {
+      printf("retry: expected=%d\trxed=%d\n",expected_id,rxmsg->id);
+      tries++;
+    }
+  }
+  return -1;
+}
+
+void sendhelper(ltcomm_net_ctrl_blk_t       *pnet,
+                ltcomm_client_request_msg_t *txmsg,
+                ltcomm_server_reply_msg_t   *rxmsg,
+                uint8_t                      expected_id)
+{
+  char s[64];
+  LTCOMM_CID2STR(txmsg->id,s)
+    printf("********************\nsending msg w/ ID=%s\n", s);
+  if (ltclient_send_msg_with_retries(pnet,txmsg,rxmsg,expected_id) == -1){
+    printf("failed to send msg w/ ID=%s\n", s);
+    exit(1);
+  }
+  printf("received expected reponse to msg w/ ID=%s\n", s);
+}
 
 int main(void)
 {
-  int                         sd;
-  struct  sockaddr_in         server_addr;
-  struct  sockaddr_in         client_addr;
-  npcomm_client_request_msg_t txmsg;
-  npcomm_server_reply_msg_t   rxmsg;
+  ltcomm_net_ctrl_blk_t       net;
+  ltcomm_client_request_msg_t txmsg;
+  ltcomm_server_reply_msg_t   rxmsg;
 
   /*****************************************************************************/
   /* setup network */
   /*****************************************************************************/
-  if ((sd=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+  if ((net.sd=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
     printf("Failed to create socket descriptor\n");
     printf("errno: %d\n",errno);
     printf("Socket error: %s\n",strerror(errno));
   }
 
-  memset(&(server_addr), 0, /* Zero the address struct first */
-         sizeof((server_addr)));
-  server_addr.sin_family      = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(LTCOMM_DEFAULT_HOST);
-  server_addr.sin_port        = htons(LTCOMM_DEFAULT_SERVER_PORT);
+  memset(&(net.server_addr), 0, /* Zero the address struct first */
+         sizeof((net.server_addr)));
+  net.server_addr.sin_family      = AF_INET;
+  net.server_addr.sin_addr.s_addr = inet_addr(LTCOMM_DEFAULT_HOST);
+  net.server_addr.sin_port        = htons(LTCOMM_DEFAULT_SERVER_PORT);
 
-  memset(&(client_addr), 0, /* Zero the address struct first */
-         sizeof((client_addr)));
+  memset(&(net.client_addr), 0, /* Zero the address struct first */
+         sizeof((net.client_addr)));
   /* bind any port */
-  client_addr.sin_family           = AF_INET;
-  client_addr.sin_addr.s_addr      = inet_addr(LTCOMM_DEFAULT_HOST);
-  client_addr.sin_port             = htons(LTCOMM_DEFAULT_CLIENT_PORT);
-  if (bind(sd,
-           (struct sockaddr *) &(client_addr),
-           sizeof((client_addr))) == -1) {
+  net.client_addr.sin_family           = AF_INET;
+  net.client_addr.sin_addr.s_addr      = inet_addr(LTCOMM_DEFAULT_HOST);
+  net.client_addr.sin_port             = htons(LTCOMM_DEFAULT_CLIENT_PORT);
+  if (bind(net.sd,
+           (struct sockaddr *) &(net.client_addr),
+           sizeof((net.client_addr))) == -1) {
     printf("Failed to bind to the socket specified descriptor\n");
     printf("errno: %d\n",errno);
     printf("Socket error: %s\n",strerror(errno));
@@ -51,201 +135,46 @@ int main(void)
   /*****************************************************************************/
   /* send setup packets */
   /*****************************************************************************/
-  bool acked = false;
 
-  while (!acked) {
-    txmsg.id                         = LTCOMM_CID_CONNECT;
-    printf("sending connect request\n");
-    if (sendto(sd, (void *) &txmsg, sizeof(npcomm_client_request_msg_t), 0,
-               (struct sockaddr *) &server_addr,
-               sizeof(server_addr)) == -1) {
-      printf("Network sendto failed: id=%d\n", txmsg.id);
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    rxmsg.id                                  = LTCOMM_SID_ACK;
-    int client_slen                           = sizeof(struct sockaddr_in);
-    if (recvfrom(sd, (void *) &rxmsg, sizeof(npcomm_server_reply_msg_t), 0,
-                 (struct sockaddr *)&(client_addr),
-                 (socklen_t * )&client_slen) == -1) {
-      printf("Network recvfrom failed\n");
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    else {
-      if (rxmsg.id == LTCOMM_SID_ACK) {
-        acked = true;
-        printf("received connect ack\n");
-      }
-    }
-  }
+  txmsg.id = LTCOMM_CID_CONNECT;
+  sendhelper(&net, &txmsg, &rxmsg, LTCOMM_SID_ACK);
 
-  acked                              = false;
-  while (!acked) {
-    txmsg.id                         = LTCOMM_CID_QUERY_VERSION;
-    printf("sending query version\n");
-    if (sendto(sd, (void *) &txmsg, sizeof(npcomm_client_request_msg_t), 0,
-               (struct sockaddr *) &server_addr,
-               sizeof(server_addr)) == -1) {
-      printf("Network sendto failed: id=%d\n", txmsg.id);
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    rxmsg.id                                  = LTCOMM_SID_ACK;
-    int client_slen                           = sizeof(struct sockaddr_in);
-    if (recvfrom(sd, (void *) &rxmsg, sizeof(npcomm_server_reply_msg_t), 0,
-                 (struct sockaddr *)&(client_addr),
-                 (socklen_t * )&client_slen) == -1) {
-      printf("Network recvfrom failed\n");
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    else {
-      if (rxmsg.id == LTCOMM_SID_VERSION_REPLY) {
-        acked = true;
-        printf("received version reply = %x\n", rxmsg.body.nondata_body);
-      }
-    }
-  }
+  txmsg.id = LTCOMM_CID_QUERY_VERSION;
+  sendhelper(&net, &txmsg, &rxmsg, LTCOMM_SID_VERSION_REPLY);
 
-  acked                              = false;
-  while (!acked) {
-    txmsg.id                         = LTCOMM_CID_START_DATA;
-    printf("sending start data\n");
-    if (sendto(sd, (void *) &txmsg, sizeof(npcomm_client_request_msg_t), 0,
-               (struct sockaddr *) &server_addr,
-               sizeof(server_addr)) == -1) {
-      printf("Network sendto failed: id=%d\n", txmsg.id);
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    rxmsg.id                                  = LTCOMM_SID_ACK;
-    int client_slen                           = sizeof(struct sockaddr_in);
-    if (recvfrom(sd, (void *) &rxmsg, sizeof(npcomm_server_reply_msg_t), 0,
-                 (struct sockaddr *)&(client_addr),
-                 (socklen_t * )&client_slen) == -1) {
-      printf("Network recvfrom failed\n");
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    else {
-      if (rxmsg.id == LTCOMM_SID_ACK) {
-        acked = true;
-        printf("received start data ack\n");
-      }
-    }
-  }
+  txmsg.id = LTCOMM_CID_START_DATA;
+  sendhelper(&net, &txmsg, &rxmsg, LTCOMM_SID_ACK);
 
-
-  acked                              = false;
-  while (!acked) {
-    txmsg.id                         = LTCOMM_CID_REGISTER_PROGRAM_PROFILE_ID;
-    printf("sending register program profile id\n");
-    if (sendto(sd, (void *) &txmsg, sizeof(npcomm_client_request_msg_t), 0,
-               (struct sockaddr *) &server_addr,
-               sizeof(server_addr)) == -1) {
-      printf("Network sendto failed: id=%d\n", txmsg.id);
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    rxmsg.id                                  = LTCOMM_SID_ACK;
-    int client_slen                           = sizeof(struct sockaddr_in);
-    if (recvfrom(sd, (void *) &rxmsg, sizeof(npcomm_server_reply_msg_t), 0,
-                 (struct sockaddr *)&(client_addr),
-                 (socklen_t * )&client_slen) == -1) {
-      printf("Network recvfrom failed\n");
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    else {
-      if (rxmsg.id == LTCOMM_SID_ACK) {
-        acked = true;
-        printf("received register program profile id ack\n");
-      }
-    }
-  }
+  txmsg.id = LTCOMM_CID_REGISTER_PROGRAM_PROFILE_ID;
+  sendhelper(&net, &txmsg, &rxmsg, LTCOMM_SID_ACK);
 
   /*****************************************************************************/
   /* mainloop */
   /*****************************************************************************/
   int i;
-  for (i=0; i<20; i++) {
-    acked                              = false;
-    while (!acked) {
-      txmsg.id                         = LTCOMM_CID_GET_DATA;
-      printf("sending get data\n");
-      if (sendto(sd, (void *) &txmsg, sizeof(npcomm_client_request_msg_t), 0,
-                 (struct sockaddr *) &server_addr,
-                 sizeof(server_addr)) == -1) {
-        printf("Network sendto failed: id=%d\n", txmsg.id);
-        printf("errno: %d\n",errno);
-        printf("read error: %s\n",strerror(errno));
-        return -1;
+  for (i=0; i<5; i++) {
+    printf("********************\nsending get data\n");
+    txmsg.id                                                 = LTCOMM_CID_GET_DATA;
+    if (ltclient_send_msg_and_get_reply(&net,&txmsg,&rxmsg) != -1) {
+      if (rxmsg.id == LTCOMM_SID_DATA_REPLY) {
+        printf("received get data reply:\n");
+        printf("  heading: %f\tpitch: %f\troll: %f\n",
+               rxmsg.body.data_body.heading,
+               rxmsg.body.data_body.pitch,
+               rxmsg.body.data_body.roll);
       }
-      rxmsg.id                                  = LTCOMM_SID_ACK;
-      int client_slen                           = sizeof(struct sockaddr_in);
-      if (recvfrom(sd, (void *) &rxmsg, sizeof(npcomm_server_reply_msg_t), 0,
-                   (struct sockaddr *)&(client_addr),
-                   (socklen_t * )&client_slen) == -1) {
-        printf("Network recvfrom failed\n");
-        printf("errno: %d\n",errno);
-        printf("read error: %s\n",strerror(errno));
-        return -1;
-      }
-      else {
-        if (rxmsg.id == LTCOMM_SID_DATA_REPLY) {
-          acked = true;
-          printf("received get data reply:\n");
-          printf("  heading: %f\tpitch: %f\troll: %f\n",
-                 rxmsg.body.get_data_body.heading,
-                 rxmsg.body.get_data_body.pitch,
-                 rxmsg.body.get_data_body.roll);
-        }
-      }
-      sleep(1);
     }
+    sleep(1);
   }
 
   /*****************************************************************************/
   /* shutdown */
   /*****************************************************************************/
-  acked                              = false;
-  while (!acked) {
-    txmsg.id                         = LTCOMM_CID_DISCONNECT;
-    printf("sending disconnect\n");
-    if (sendto(sd, (void *) &txmsg, sizeof(npcomm_client_request_msg_t), 0,
-               (struct sockaddr *) &server_addr,
-               sizeof(server_addr)) == -1) {
-      printf("Network sendto failed: id=%d\n", txmsg.id);
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    rxmsg.id                                  = LTCOMM_SID_ACK;
-    int client_slen                           = sizeof(struct sockaddr_in);
-    if (recvfrom(sd, (void *) &rxmsg, sizeof(npcomm_server_reply_msg_t), 0,
-                 (struct sockaddr *)&(client_addr),
-                 (socklen_t * )&client_slen) == -1) {
-      printf("Network recvfrom failed\n");
-      printf("errno: %d\n",errno);
-      printf("read error: %s\n",strerror(errno));
-      return -1;
-    }
-    else {
-      if (rxmsg.id == LTCOMM_SID_ACK) {
-        acked = true;
-        printf("received disconnect ack\n");
-      }
-    }
-  }
+  txmsg.id = LTCOMM_CID_DISCONNECT;
+  sendhelper(&net, &txmsg, &rxmsg, LTCOMM_SID_ACK);
+
+  txmsg.id = LTCOMM_CID_TERMINATE;
+  sendhelper(&net, &txmsg, &rxmsg, LTCOMM_SID_ACK);
 
   return 0;
 }
