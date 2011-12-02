d5a39225e595a74e9bd8b7ff45dfac5cc9623f37
f.josef <f.josef@email.cz>
2011-12-02 21:42:29 +0000

    Added readme files for development package and Xplane plugin package. Fixed problems with Mac compilation.

--- src/pref_flex.c
+++ src/pref_flex.c
@@ -53,6 +53,7 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
+#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -82,8 +83,6 @@ typedef unsigned int flex_uint32_t;
 #ifndef UINT32_MAX
 #define UINT32_MAX             (4294967295U)
 #endif
-
-#endif /* ! C99 */
 
 #endif /* ! FLEXINT_H */
 
@@ -141,15 +140,7 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k.
- * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
- * Ditto for the __ia64__ case accordingly.
- */
-#define YY_BUF_SIZE 32768
-#else
 #define YY_BUF_SIZE 16384
-#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -478,7 +469,7 @@ char *yytext;
   extern int line_num;
 #define YY_NO_INPUT 1
 
-#line 482 "pref_flex.c"
+#line 473 "pref_flex.c"
 
 #define INITIAL 0
 #define VALUE 1
@@ -559,12 +550,7 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k */
-#define YY_READ_BUF_SIZE 16384
-#else
 #define YY_READ_BUF_SIZE 8192
-#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -583,7 +569,7 @@ static int input (void );
 	if ( YY_CURRENT_BUFFER_LVALUE->yy_is_interactive ) \
 		{ \
 		int c = '*'; \
-		size_t n; \
+		unsigned n; \
 		for ( n = 0; n < max_size && \
 			     (c = getc( yyin )) != EOF && c != '\n'; ++n ) \
 			buf[n] = (char) c; \
@@ -667,7 +653,7 @@ YY_DECL
     
 #line 16 "pref_flex.l"
 
-#line 671 "pref_flex.c"
+#line 657 "pref_flex.c"
 
 	if ( !(yy_init) )
 		{
@@ -823,7 +809,7 @@ YY_RULE_SETUP
 #line 49 "pref_flex.l"
 ECHO;
 	YY_BREAK
-#line 827 "pref_flex.c"
+#line 813 "pref_flex.c"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(VALUE):
 case YY_STATE_EOF(SEC_NAME):
@@ -1546,8 +1532,8 @@ YY_BUFFER_STATE yy_scan_string (yyconst char * yystr )
 
 /** Setup the input buffer state to scan the given bytes. The next call to yylex() will
  * scan from a @e copy of @a bytes.
- * @param yybytes the byte buffer to scan
- * @param _yybytes_len the number of bytes in the buffer pointed to by @a bytes.
+ * @param bytes the byte buffer to scan
+ * @param len the number of bytes in the buffer pointed to by @a bytes.
  * 
  * @return the newly allocated buffer state object.
  */
