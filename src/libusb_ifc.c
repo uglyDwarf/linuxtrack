357bac3f0edad470354b90a12867d69807ed89f7
f.josef <f.josef@email.cz>
2012-02-19 18:35:28 +0000

    Track IR initialization (mainly TIR4) is now much more verbose. Linuxtrack server now times out after one minute. TrackIR 4 initialization should now handle less responsive TrackIR 4 devices.

--- src/libusb_ifc.c
+++ src/libusb_ifc.c
@@ -12,26 +12,34 @@ static bool interface_claimed = false;
 
 bool ltr_int_init_usb()
 {
+  ltr_int_log_message("Initializing libusb.\n");
   int res = libusb_init(&usb_context);
   if(res != 0){
     ltr_int_log_message("Problem initializing libusb!\n");
     return false;
   }
+  ltr_int_log_message("Libusb initialized successfuly.\n");
   
   libusb_set_debug(usb_context, 0);
+  ltr_int_log_message("Libusb debug level set.\n");
   return true;
 }
 
 static bool is_tir(libusb_device *dev, unsigned int devid)
 {
   struct libusb_device_descriptor desc;
+
+  ltr_int_log_message("Checking, if device is Track IR.\n");
   if(libusb_get_device_descriptor(dev, &desc)){
     ltr_int_log_message("Error getting device descriptor!\n");
     return false;
   }
+  ltr_int_log_message("Device descriptor received.\n");
   if((desc.idVendor == 0x131D) && (desc.idProduct == devid)){
+    ltr_int_log_message("Device is a TrackIR.\n");
     return true;
   }else{
+    ltr_int_log_message("Device is not a TrackIR.\n");
     return false;
   }
 }
@@ -43,7 +51,10 @@ dev_found ltr_int_find_tir(unsigned int devid)
   libusb_device **list;
   libusb_device *found = NULL;
   dev_found dev = NONE;
+  
+  ltr_int_log_message("Requesting device list.\n");
   ssize_t cnt = libusb_get_device_list(usb_context, &list);
+  ltr_int_log_message("Device list received (%d devices).\n", cnt);
   ssize_t i = 0;
   int err = 0;
   if (cnt < 0){
@@ -105,15 +116,19 @@ dev_found ltr_int_find_tir(unsigned int devid)
   }
 
   if(found){
+    ltr_int_log_message("Opening handle to the device found.\n");
     err = libusb_open(found, &handle);
     if(err){
       ltr_int_log_message("Error opening device!\n");
       return NONE;
     }
+    ltr_int_log_message("Handle opened successfully.\n");
   }else{
-    ltr_int_log_message("Can't find TrackIR!\n");
+    ltr_int_log_message("Can't find any TrackIR!\n");
   }
+  ltr_int_log_message("Freeing device list.\n");
   libusb_free_device_list(list, 1);
+  ltr_int_log_message("Device list freed.\n");
   if(handle != NULL){
     return dev;
   }else{
@@ -125,40 +140,51 @@ dev_found ltr_int_find_tir(unsigned int devid)
 static bool configure_tir(int config)
 {
   int cfg = -1;
+  ltr_int_log_message("Requesting TrackIR configuration.\n");
   if(libusb_get_configuration(handle, &cfg)){
     ltr_int_log_message("Can't get device configuration!\n");
     return false;
   }
+  ltr_int_log_message("TrackIR configuration received.\n");
   if(cfg != config){
+    ltr_int_log_message("Trying to set TrackIR configuration.\n");
     if(libusb_set_configuration(handle, config)){
       ltr_int_log_message("Can't set device configuration!\n");
       return false;
     }
+    ltr_int_log_message("TrackIR configured.\n");
+  }else{
+    ltr_int_log_message("TrackIR already in requested configuration.\n");
   }
   return true;
 }
 
 static bool claim_tir(int config, unsigned int interface)
 {
+  ltr_int_log_message("Trying to claim TrackIR interface.\n");
   if(libusb_claim_interface(handle, interface)){
     ltr_int_log_message("Couldn't claim interface!\n");
     interface_claimed = false;
     return false;
   }
+  ltr_int_log_message("TrackIR interface claimed.\n");
   interface_claimed = true;
 //  if(libusb_reset_device(handle)){
 //    log_message("Couldn't reset device!\n");
 //    return false;
 //  }
   int cfg;
+  ltr_int_log_message("Requesting TrackIR configuration.\n");
   if(libusb_get_configuration(handle, &cfg)){
     ltr_int_log_message("Can't get device configuration!\n");
     return false;
   }
+  ltr_int_log_message("TrackIR configuration received.\n");
   if(cfg != config){
-    ltr_int_log_message("Can't set device configuration!\n");
+    ltr_int_log_message("Device configuration is wrong!\n");
     return false;
   }
+  ltr_int_log_message("Device configuration is OK.\n");
   return true;
 }
 
@@ -192,6 +218,8 @@ bool ltr_int_receive_data(unsigned char data[], size_t size, size_t *transferred
     if(res != LIBUSB_ERROR_TIMEOUT){
       ltr_int_log_message("Problem reading data from TIR! %d - %d transferred\n", res, *transferred);
       return false;
+    }else{
+      ltr_int_log_message("Data receive request timed out!\n");
     }
   }
   return true;
@@ -199,11 +227,17 @@ bool ltr_int_receive_data(unsigned char data[], size_t size, size_t *transferred
 
 void ltr_int_finish_usb(unsigned int interface)
 {
+  ltr_int_log_message("Closing TrackIR.\n");
   if(interface_claimed){
+    ltr_int_log_message("Releasing TrackIR interface.\n");
     libusb_release_interface(handle, interface);
+    ltr_int_log_message("TrackIR interface released.\n");
   }
   interface_claimed = false;
+  ltr_int_log_message("Closing TrackIR handle.\n");
   libusb_close(handle);
+  ltr_int_log_message("Exiting libusb.\n");
   libusb_exit(usb_context);
+  ltr_int_log_message("Libusb exited.\n");
 }
 
