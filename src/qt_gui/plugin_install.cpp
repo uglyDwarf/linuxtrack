c82ea115d63bd3a9f29dd84690ea733dad9aa3bf
f.josef <f.josef@email.cz>
2012-09-19 19:12:28 +0000

    Wine bridge now doesn't depend on the new TIR firmware package.

--- src/qt_gui/plugin_install.cpp
+++ src/qt_gui/plugin_install.cpp
@@ -3,6 +3,14 @@
 #include <iostream>
 #include <QProcess>
 #include <QFileDialog>
+#include <QMessageBox>
+#include <zlib.h>
+#include "dlfirmware.h"
+#include "utils.h"
+
+const QString PluginInstall::keySrc(PREF.getRsrcDirPath() + "/tir_firmware/tir4.fw.gz");
+const QString PluginInstall::keyFile(PREF.getRsrcDirPath() + "/tir_firmware/sig.key");
+const QString PluginInstall::sigFile(PREF.getRsrcDirPath() + "/tir_firmware/sig.bin");
 
 PluginInstall::PluginInstall(const Ui::LinuxtrackMainForm &ui):
   gui(ui), inst(NULL)
@@ -16,6 +24,10 @@ PluginInstall::PluginInstall(const Ui::LinuxtrackMainForm &ui):
 
 PluginInstall::~PluginInstall()
 {
+  if(dlfw != NULL){
+    delete dlfw;
+    dlfw = NULL;
+  }
   delete inst;
 }
 
@@ -31,7 +43,70 @@ void PluginInstall::instFinished(int exitCode, QProcess::ExitStatus exitStatus)
 {
   (void) exitCode;
   (void) exitStatus;
+  if(!QFile::exists(keySrc)){
+    printf("Key source not existing!\n");
+    tirFirmwareInstall();
+  }else{
+    printf("We have key source, continue with extraction...\n");
+    tirFirmwareInstalled(false);
+  }
   gui.pushButton_2->setEnabled(true);
+}
+
+void PluginInstall::tirFirmwareInstall()
+{
+  if(dlfw == NULL){
+    dlfw = new dlfwGui();
+    QObject::connect(dlfw, SIGNAL(finished(bool)),
+      this, SLOT(tirFirmwareInstalled(bool)));
+  }
+  dlfw->show();
+}
+
+static bool saveKeyFile(const char *fname, const char *target)
+{
+  bool res = false;
+  uint8_t buf[400];
+  gzFile gf;
+  if((gf = gzopen(fname, "rb")) == NULL){
+    ltr_int_log_message("Couldn't open keyfile '%s'\n", fname);
+    return false;
+  }
+  size_t read = gzread(gf, buf, sizeof(buf));
+  res = (read == sizeof(buf));
+  gzclose(gf);
+  if(res == false){
+    return false;
+  }
+  res = false;
+  for(unsigned int i = 0; i < sizeof(buf); ++i) buf[i] ^= 0x5A;
+  FILE *f = fopen(target, "wb");
+  if(f != NULL){
+    res = (fwrite(buf, 1, sizeof(buf), f) == sizeof(buf));
+    fclose(f);
+  }else{
+    ltr_int_log_message("Can't open target file '%s'!\n", target);
+  }
+  return res;
+}
+
+bool PluginInstall::isTirFirmwareInstalled()
+{
+  return QFile::exists(keyFile) || QFile::exists(sigFile);
+}
+
+void PluginInstall::tirFirmwareInstalled(bool ok)
+{
+  (void) ok;
+  bool res = false;
+  if(QFile::exists(keySrc) && (!QFile::exists(sigFile))){
+    res = saveKeyFile(qPrintable(keySrc), qPrintable(keyFile));
+  }
+  if(!(res || QFile::exists(sigFile))){
+    QMessageBox::warning(NULL, "TrackIR firmware install problem", 
+"TrackIR firmware package was not installed, without it\n\
+the linuxtrack-wine bridge will not be fully functional!", QMessageBox::Ok);
+  }
 }
 
 void PluginInstall::installWinePlugin()
