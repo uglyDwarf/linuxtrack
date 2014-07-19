9df8a7bd28f3451a40819e50f9efb80c4dfab5c1
f.josef@email.cz <f.josef@email.cz@733d3608-6b34-11de-ba0c-230037762ff9>
2014-07-19 20:58:24 +0000

    Wine warning can be made to stop bothering.

---
+++ trunk/src/qt_gui/wine_warn.h
@@ -0,0 +1,20 @@
+#ifndef WINE_WARN__H
+#define WINE_WARN__H
+
+#include <QWidget>
+#include "ui_wine_warn.h"
+
+class WineWarn : public QDialog
+{
+ Q_OBJECT
+ public:
+  WineWarn(QWidget *parent);
+  ~WineWarn();
+ private:
+  Ui::Dialog ui;
+ public slots:
+  void show();
+  void on_OKButton_pressed();
+};
+
+#endif
