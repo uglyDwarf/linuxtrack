ae16b88237d0eda201cad7dae68e7c1e3161fe57
f.josef@email.cz <f.josef@email.cz@733d3608-6b34-11de-ba0c-230037762ff9>
2014-08-30 03:12:35 +0000

    Fixing problems flagged by new automake (subdir-objects).

--- trunk/src/pref_bison.h
+++ trunk/src/pref_bison.h
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.7.12-4996.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
 /* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
-   
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,13 +26,13 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
 #ifndef YY_LTR_INT_PARSER_PREF_BISON_HPP_INCLUDED
 # define YY_LTR_INT_PARSER_PREF_BISON_HPP_INCLUDED
-/* Enabling traces.  */
+/* Debug traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 1
 #endif
@@ -40,21 +40,20 @@
 extern int ltr_int_parser_debug;
 #endif
 
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     TOKEN_COMMENT = 258,
-     TOKEN_LEFT_BRACKET = 259,
-     TOKEN_RIGHT_BRACKET = 260,
-     TOKEN_KEY = 261,
-     TOKEN_EQ = 262,
-     TOKEN_VALUE = 263,
-     TOKEN_SECNAME = 264,
-     LOW_PRIO = 265
-   };
+  enum yytokentype
+  {
+    TOKEN_COMMENT = 258,
+    TOKEN_LEFT_BRACKET = 259,
+    TOKEN_RIGHT_BRACKET = 260,
+    TOKEN_KEY = 261,
+    TOKEN_EQ = 262,
+    TOKEN_VALUE = 263,
+    TOKEN_SECNAME = 264,
+    LOW_PRIO = 265
+  };
 #endif
 /* Tokens.  */
 #define TOKEN_COMMENT 258
@@ -66,54 +65,40 @@ extern int ltr_int_parser_debug;
 #define TOKEN_SECNAME 264
 #define LOW_PRIO 265
 
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
 {
-/* Line 2053 of yacc.c  */
-#line 16 "pref_bison.ypp"
+#line 16 "pref_bison.ypp" /* yacc.c:1909  */
 
   std::string *str;
   keyVal *kv;
   section *sec;
   prefs *prf;
 
-
-/* Line 2053 of yacc.c  */
-#line 85 "pref_bison.hpp"
-} YYSTYPE;
+#line 81 "pref_bison.hpp" /* yacc.c:1909  */
+};
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+/* Location type.  */
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
 {
   int first_line;
   int first_column;
   int last_line;
   int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+};
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
 
 
-#ifdef YYPARSE_PARAM
-#if defined __STDC__ || defined __cplusplus
-int ltr_int_parser_parse (void *YYPARSE_PARAM);
-#else
-int ltr_int_parser_parse ();
-#endif
-#else /* ! YYPARSE_PARAM */
-#if defined __STDC__ || defined __cplusplus
+
 int ltr_int_parser_parse (prefs *prf);
-#else
-int ltr_int_parser_parse ();
-#endif
-#endif /* ! YYPARSE_PARAM */
 
 #endif /* !YY_LTR_INT_PARSER_PREF_BISON_HPP_INCLUDED  */
